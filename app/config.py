f84336689457cfa886f8556a30a5003f37999598
Subham24Singh <subhamsinght2234@gmail.com>
2025-09-16 18:04:36 +0530

    Initial commit of IHMP_MVP project

--- app/config.py
+++ app/config.py
@@ -5,8 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL = "postgresql://postgres:Pi%40122912@localhost:5432/ihmp_db"
-    TIMEZONE: str = os.getenv("TIMEZONE", "Asia/Kolkata") 
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:Manu2413@localhost:5432/ihmp_db")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "testsecret")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+    TIMEZONE: str = os.getenv("TIMEZONE", "Asia/Kolkata")
 
 # Instantiate the settings
 settings = Settings()
